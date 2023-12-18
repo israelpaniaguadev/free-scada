b97e9ee0cdb109124c3508f6e0c163cf3a3e50ef
Jakku Night <j97k107u45k117n@gmail.com>
2023-12-18 06:55:10 -0300

    Added new UI components.

--- src/ui/src/App.tsx
+++ src/ui/src/App.tsx
@@ -1,11 +1,19 @@
+import Modal from "./components/Modal";
+import Text from "./components/Text";
+
+
 const App = () => {
 	return (
 		<div
 			className={
-				"w-full min-h-screen m-0 p-4 flex-1 flex-col items-center justify-center gap-4 bg-black text-gray-300"
+				"w-screen h-screen m-0 p-4 flex items-center justify-center gap-4 bg-black text-gray-300"
 			}
 		>
-
+			<Modal openBtn="Login" title="Alert">
+				<Text>
+					Want to close the program?
+				</Text>
+			</Modal>
 		</div>
 	);
 };
