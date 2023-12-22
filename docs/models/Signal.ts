159d68ba5ad7ceb73343e03f1ee2b56e5e276da6
C. Israel Paniagua P <130411364+israelpaniaguadev@users.noreply.github.com>
2023-12-22 15:05:19 -0300

    Update Signal.ts
    Signed-off-by: C. Israel Paniagua P. <130411364+israelpaniaguadev@users.noreply.github.com>

--- docs/models/Signal.ts
+++ docs/models/Signal.ts
@@ -1,6 +1,9 @@
 export class Signal {
 	constructor(
 		private name: string, // The name of the signal.
-		private description: string // The description of what the signal means.
+		private description: string, // The description of what the signal means.
+		private quality: number, // The signal quality.
+		private statusValue: number | boolean, // The signal value.
+		private isError: boolean // If has an error.
 	) { }
 };
