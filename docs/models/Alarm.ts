0d911a35a56454ec11207f45cfc76770cf740e4b
Jakku Night <R101g117l117s@gmail.com>
2023-12-26 10:41:47 +0000

    Decoupuling all the models to apply the composition pattern.

--- docs/models/Alarm.ts
+++ docs/models/Alarm.ts
@@ -1,9 +1,6 @@
-import { Device } from "./Device";
-
 export class Alarm {
 	constructor(
 		private name: string, // An alarm needs an identifier
 		private description: string, // An alarm needs a description to make easier to operate.
-		private device: Device, // The device that produces the alarm.
 	) { }
 };
--- docs/models/State.ts
+++ docs/models/State.ts
@@ -1,9 +1,6 @@
-import { Device } from "./Device";
-
 export class State {
 	constructor(
 		private name: string, // The state name.
 		private description: string, // A short description of what the state means.
-		private device: Device // The device that produces the signal.
 	) { }
 };
