32bab436b8cf491cfc0fb24beb2c312d0fadfbf5
Jakku Night <j97k107u45k117n@gmail.com>
2024-01-08 10:42:40 -0300

    Testing deserialization for Numbers...

--- docs/protocols/Tester.test.ts
+++ docs/protocols/Tester.test.ts
@@ -1,5 +1,6 @@
 import { describe, test, expect } from "bun:test";
 import { deserializeNumber } from "./deserializeNumber";
+import { serializeNumber } from "./serializeNumber";
 
 describe("deserializeNumber()", function () {
   test("(ArrayBuffer(1)[0]) => 0", function () {
@@ -12,14 +13,24 @@ describe("deserializeNumber()", function () {
   });
   test(`(ArrayBuffer(2)[4, 0]) => 1024`, function () {
     let input = new Uint16Array([1024]).buffer as ArrayBuffer;
-    expect(deserializeNumber(input)).toEqual(1024);
+    expect(deserializeNumber(input, false, true)).toEqual(1024);
   });
   test(`(ArrayBuffer(4)[0, 0, 4, 0]) => 1024`, function () {
     let input = new Uint32Array([1024]).buffer as ArrayBuffer;
-    expect(deserializeNumber(input)).toEqual(1024);
+    expect(deserializeNumber(input, false, true)).toEqual(1024);
   });
   test(`(ArrayBuffer(4)[1, 0, 4, 0], true) => 1024`, function () {
     let input = new Int32Array([-1024]).buffer as ArrayBuffer;
+    expect(deserializeNumber(input, true, true)).toEqual(-1024);
+  });
+  test(`Should be the inverse of 'serializeNumber()'`, function () {
+    let input = serializeNumber(-1024, 4);
+    let output = deserializeNumber(
+      new Uint32Array([-1024]).buffer as ArrayBuffer
+    );
     expect(deserializeNumber(input, true)).toEqual(-1024);
+    expect(serializeNumber(output, 4)).toEqual(
+      new Uint32Array([-1024]).buffer as ArrayBuffer
+    );
   });
 });
--- docs/protocols/deserializeNumber.ts
+++ docs/protocols/deserializeNumber.ts
@@ -1,7 +1,7 @@
 export function deserializeNumber(
   buffer: ArrayBuffer,
   signed: boolean = false,
-  endianness: boolean = true
+  littleEndian: boolean = false
 ): number {
   let view = new DataView(buffer);
   if (!signed) {
@@ -9,20 +9,20 @@ export function deserializeNumber(
       return view.getUint8(0);
     }
     if (buffer.byteLength == 2) {
-      return view.getUint16(0, endianness);
+      return view.getUint16(0, littleEndian);
     }
     if (buffer.byteLength == 4) {
-      return view.getUint32(0, endianness);
+      return view.getUint32(0, littleEndian);
     }
   }
   if (buffer.byteLength == 1) {
     return view.getInt8(0);
   }
   if (buffer.byteLength == 2) {
-    return view.getInt16(0, endianness);
+    return view.getInt16(0, littleEndian);
   }
   if (buffer.byteLength == 4) {
-    return view.getInt32(0, endianness);
+    return view.getInt32(0, littleEndian);
   }
   return 0;
 }
