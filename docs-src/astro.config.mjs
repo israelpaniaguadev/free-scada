d7b6c31ac75ae9f950728ce37e5d8fd72e01236e
Jakku Night <R101g117l117s@gmail.com>
2024-01-15 08:00:27 -0300

    Building new docs with Astro/Starlight...

---
+++ docs-src/astro.config.mjs
@@ -0,0 +1,27 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Free SCADA Docs',
+			social: {
+				github: 'https://github.com/jakkunight/free-scada',
+			},
+			sidebar: [
+				{
+					label: 'Guides',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{ label: 'Get Started', link: '/guides/get-started/' },
+					],
+				},
+				{
+					label: 'API Reference',
+					autogenerate: { directory: 'api' },
+				},
+			],
+		}),
+	],
+});
